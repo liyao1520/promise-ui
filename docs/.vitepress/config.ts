e6bf698108237df34de6bca704dc50764ab3ae8d
浅陌丿 <1520190873@qq.com>
2022-05-13 21:17:11 +0800

    fix: 修改组件库文档样式

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,39 +1,48 @@
+import { UserConfig } from 'vitepress'
+// 这里可以使用 markdown-it 插件，vitepress-theme-demoblock就是基于此开发的
+const { demoBlockPlugin } = require('vitepress-theme-demoblock')
 const sidebar = {
-  "/": [
-    { text: "快速开始", link: "/" },
+  '/': [
+    { text: '快速开始', link: '/' },
     {
-      text: "通用",
-      children: [{ text: "Button 按钮", link: "/components/button/" }],
+      text: '通用',
+      children: [{ text: 'Button 按钮', link: '/components/button/' }]
     },
     {
-      text: "导航",
+      text: '导航'
     },
     {
-      text: "反馈",
+      text: '反馈'
     },
     {
-      text: "数据录入",
+      text: '数据录入'
     },
     {
-      text: "数据展示",
+      text: '数据展示'
     },
     {
-      text: "布局",
-    },
-  ],
-};
-
-const config = {
+      text: '布局'
+    }
+  ]
+}
+const nav = [
+  { text: 'Github', link: 'https://github.com/liyao1520/promise-ui', target: '_blank', rel: '' }
+]
+const config: UserConfig = {
   themeConfig: {
     sidebar,
+    nav,
+    search: true,
+    logo: '/logo.svg'
   },
+  title: 'PromiseUI',
+  lang: 'zh-CN',
+  description: '一个vue3组件库',
   markdown: {
     config: (md) => {
-      // 这里可以使用 markdown-it 插件，vitepress-theme-demoblock就是基于此开发的
-      const { demoBlockPlugin } = require("vitepress-theme-demoblock");
-      md.use(demoBlockPlugin);
-    },
-  },
-};
+      md.use(demoBlockPlugin)
+    }
+  }
+}
 
-export default config;
+export default config
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
-import Theme from "vitepress/dist/client/theme-default";
-import Button from "../../../promiseui/button/button";
+import Theme from '../theme-default'
+import Button from '../../../promiseui/button'
 
 // 主题样式
-import "vitepress-theme-demoblock/theme/styles/index.css";
+import 'vitepress-theme-demoblock/theme/styles/index.css'
 
 // 插件的组件，主要是demo组件
-import { registerComponents } from "./register-components.js";
+import { registerComponents } from './register-components.js'
 export default {
   ...Theme,
   enhanceApp({ app }) {
-    app.component("p-button", Button);
-    registerComponents(app);
-  },
-};
+    app.component('p-button', Button)
+    registerComponents(app)
+  }
+}
