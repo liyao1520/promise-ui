6590e82bae5a86aa80d42a5f645733cb893c9c9a
浅陌丿 <1520190873@qq.com>
2022-06-21 16:08:05 +0800

    fix: 黑暗模式滚动条颜色以及修改animation css 样式

--- promiseui/tag/index.ts
+++ promiseui/tag/index.ts
@@ -1,16 +1,15 @@
-import type { App } from 'vue';
-import Tag from './src/tag';
+import type { App } from 'vue'
+import Tag from './src/tag'
 
-export * from './src/tag-types';
+export * from './src/tag-types'
 
-export { Tag };
+export { Tag }
 
 export default {
   title: 'Tag 标签',
-  category: '通用',
+  category: '数据展示',
   status: '100%',
   install(app: App): void {
-    app.component(Tag.name, Tag);
-  },
-};
-  
+    app.component(Tag.name, Tag)
+  }
+}
