7d8e54a7d432dad37a6011cf4eb3e4286a3175f8
浅陌丿 <1520190873@qq.com>
2023-01-27 22:24:41 +0800

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // import libCss from 'vite-plugin-libcss'
 const path = require('path')
@@ -11,7 +12,8 @@ export default defineConfig({
     vue(),
     vueJsx(),
     dts({
-      include: ['promiseui']
+      include: ['promiseui'],
+      outputDir: [resolve(__dirname, './dist/promiseui')]
     })
   ],
   resolve: {
