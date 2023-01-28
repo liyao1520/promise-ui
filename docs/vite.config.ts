a6bdbc6fb7cbd18921fa6aca61a4e12761b0a599
浅陌丿 <1520190873@qq.com>
2023-01-28 14:14:48 +0800

    fix: base url

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/promise-ui/',
+  base: '/',
   plugins: [vueJsx()],
   build: {
     rollupOptions: {
