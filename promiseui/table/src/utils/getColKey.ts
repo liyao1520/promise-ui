266c93908709168698088a566514059b02ce9bb1
liyao <1520190873@qq.com>
2024-03-01 13:02:20 +0800

    fix: build error

--- promiseui/table/src/utils/getColKey.ts
+++ promiseui/table/src/utils/getColKey.ts
@@ -1,9 +1,9 @@
-import { TableColumn } from '../table-types'
+import { TableColumnType } from '../table-types'
 
-export default function (col: TableColumn) {
+export default function (col: TableColumnType) {
   return col.key != undefined
     ? col.key
     : Array.isArray(col.dataIndex)
-    ? col.dataIndex.join('.')
-    : col.dataIndex
+      ? col.dataIndex.join('.')
+      : col.dataIndex
 }
